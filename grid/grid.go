f186130e748aa955a90c39994e93195eefa4b2cf
Quentin Cattez <qcattez@octo.com>
2022-06-03 11:22:40 +0200

    scratch refactoring with map and bulbstate

--- grid/grid.go
+++ grid/grid.go
@@ -7,25 +7,77 @@ type Grid interface {
 }
 
 type grid struct {
-	lights int
+	lights                   int
+	turnOffAlreadyCalledOnce bool
+	lastSquareTurnedOff      square
+	bulbs                    map[Point]bool
+}
+
+type square struct {
+	bottomRightPoint Point
+	topLeftPoint     Point
+}
+
+type Point struct {
+	absiss   int
+	ordinate int
+}
+
+func (s square) include(otherSquare square) bool {
+	return otherSquare.bottomRightPoint.absiss >= s.bottomRightPoint.absiss && otherSquare.bottomRightPoint.ordinate >= s.bottomRightPoint.ordinate && s.topLeftPoint.absiss >= otherSquare.topLeftPoint.absiss && s.topLeftPoint.absiss >= otherSquare.topLeftPoint.ordinate
+}
+
+func (s square) area() int {
+	return len(s.points())
+}
+
+func (s square) points() []Point {
+	minimumAbsiss := s.bottomRightPoint.absiss
+	minimumOrdinate := s.bottomRightPoint.ordinate
+	maximumAbsiss := s.topLeftPoint.absiss
+	maximumOrdinate := s.topLeftPoint.ordinate
+
+	var points []Point
+	for j := minimumOrdinate; j <= maximumOrdinate; j++ {
+		for i := minimumAbsiss; i <= maximumAbsiss; i++ {
+			points = append(points, Point{i, j})
+		}
+	}
+
+	return points
 }
 
 func (g *grid) TurnOff(absiss1 int, ordinate1 int, absiss2 int, ordinate2 int) {
-	g.lights = g.lights - squareArea(absiss1, ordinate1, absiss2, ordinate2)
+	currentSquare := square{Point{absiss1, ordinate1}, Point{absiss2, ordinate2}}
+	points := currentSquare.points()
+
+	for i := 0; i < len(points); i++ {
+		g.bulbs[points[i]] = false
+	}
 }
 
 func (g *grid) TurnOn(absiss1 int, ordinate1 int, absiss2 int, ordinate2 int) {
-	g.lights = squareArea(absiss1, ordinate1, absiss2, ordinate2)
-}
+	currentSquare := square{Point{absiss1, ordinate1}, Point{absiss2, ordinate2}}
+	points := currentSquare.points()
 
-func squareArea(absiss1 int, ordinate1 int, absiss2 int, ordinate2 int) int {
-	return (absiss2 - absiss1 + 1) * (ordinate2 - ordinate1 + 1)
+	for i := 0; i < len(points); i++ {
+		g.bulbs[points[i]] = true
+	}
 }
 
 func (g grid) LightsOn() int {
-	return g.lights
+	bulbsOn := 0
+	for _, bulbIsOn := range g.bulbs {
+		if bulbIsOn {
+			bulbsOn++
+		}
+	}
+
+	return bulbsOn
 }
 
 func New() Grid {
-	return &grid{}
+	grid := grid{}
+	grid.bulbs = make(map[Point]bool)
+	return &grid
 }
--- grid/grid_test.go
+++ grid/grid_test.go
@@ -86,10 +86,26 @@ var _ = Describe("Grid", func() {
 			Expect(grid.LightsOn()).To(Equal(lightsOnBeforeTurningOff - 6))
 		})
 
-		It("turn off should be idempotent", func() {
+		It("turn off should be idempotent on points", func() {
 			grid.TurnOff(0, 0, 0, 0)
 			grid.TurnOff(0, 0, 0, 0)
 			Expect(grid.LightsOn()).To(Equal(lightsOnBeforeTurningOff - 1))
+		})
+
+		It("should still turn off several points consecutively", func() {
+			grid.TurnOff(0, 0, 0, 0)
+			grid.TurnOff(1, 1, 1, 1)
+			Expect(grid.LightsOn()).To(Equal(lightsOnBeforeTurningOff - 2))
+		})
+
+		It("turn off should be idempotent on squares", func() {
+			c := make(map[Point]int)
+			c[Point{}] = 3
+			print(c[Point{0, 1}])
+
+			grid.TurnOff(1, 1, 3, 3)
+			grid.TurnOff(2, 2, 2, 2)
+			Expect(grid.LightsOn()).To(Equal(lightsOnBeforeTurningOff - 9))
 		})
 	})
 
